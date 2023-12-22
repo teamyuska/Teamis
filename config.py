b6959216c69ab0092d2c9b33f9815d838954ab30
teamyuska <118346503+teamyuska@users.noreply.github.com>
2023-12-22 11:06:54 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Infinity BOTs <https://t.me/Infinity_BOTs>
 
 import os
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID", "7948888"))
+API_HASH = os.getenv("API_HASH", "48285ac512613754f7913b310e884222")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6746110732:AAEqr0qHkncbPANl4sIyY3Zsrz08qg88tkc"
